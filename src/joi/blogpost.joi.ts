c7073f450d5521b83bcbdfbfdbda645fa1ec25bf
Vicolas11 <incrediblechamp1@gmail.com>
2022-11-01 04:05:30 +0100

    modified joi blogpost

--- src/joi/blogpost.joi.ts
+++ src/joi/blogpost.joi.ts
@@ -1,13 +1,4 @@
 import Joi from "joi";
 
+// Gideon Adama
 
-export const BlogPostInputSchema = Joi.object({
-  id: Joi.string().min(36).required(),
-  title: Joi.string().min(5).required(),
-  content: Joi.string().min(30).required(),
-  image: Joi.string().min(38).regex(/[.jpg]$/),
-});
-
-export const DelBlogPostInputSchema = Joi.object({
-  id: Joi.string().min(36).required(),
-});
